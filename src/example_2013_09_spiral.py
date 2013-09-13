a2b4ae795a9cd8639c121d50b8be350c32c309e3
Björn <bjoernwe@users.noreply.github.com>
2013-09-13 17:03:51 +0200

    working algorithm and example

--- src/example_2013_09_spiral.py
+++ src/example_2013_09_spiral.py
@@ -3,27 +3,39 @@ import numpy as np
 from matplotlib import pyplot
 from mpl_toolkits.mplot3d import Axes3D
 
+import mdp
+
 import future_preserving_map
 
 from studienprojekt.env_swiss_roll import EnvSwissRoll
 
+
 if __name__ == '__main__':
     
     N = 5000
-    env = EnvSwissRoll(sigma=0.5)
+    env = EnvSwissRoll(sigma=2.5)
+    #env = EnvSwissRollDeterministic(phi_steps=200)
     data, _, _ = env.do_random_steps(num_steps=N)
     
     # add noisy dim
-    data = np.insert(data, 2, axis=1, values=np.random.random(N))
+    noise_complete = 1. * np.random.random(N)
+    #noise_repeated = [noise_complete[i%10] for i in range(N)]
+    #data = np.insert(data, 2, axis=1, values=noise_repeated)
+    data = np.insert(data, 2, axis=1, values=noise_complete)
+    
+    # whitening
+    whitening = mdp.nodes.WhiteningNode()
+    whitening.train(data)
+    data = whitening.execute(data)
     
     # learn
-    fpm = future_preserving_map.FuturePreservingMap(output_dim=9, k=10, normalized_laplacian=True, neighbor_edges=True)
+    fpm = future_preserving_map.FuturePreservingMap(output_dim=4, k=10, normalized_laplacian=True, neighbor_edges=True)
     fpm.train(x=data)
     fpm.stop_training()
     
     # plot
-    for i in range(9):
-        ax = pyplot.subplot(3, 3, i+1, projection='3d')
+    for i in range(4):
+        ax = pyplot.subplot(2, 2, i+1, projection='3d')
         s = ax.scatter3D(xs=data[:,0], ys=data[:,1], zs=data[:,2], s=50, c=fpm.U[:,i], edgecolor='None')
         pyplot.title(fpm.E[i])
         #pyplot.colorbar(s)
--- src/future_preserving_map.py
+++ src/future_preserving_map.py
@@ -1,15 +1,118 @@
+import numpy as np
+import scipy.sparse.linalg
+import scipy.spatial.distance
+
 import mdp
 
 
-class FuturePreservingMap(mdp.Node):
+class FuturePreservingMapBase(mdp.Node):
 
-    def __init__(self, input_dim=None, dtype=None):
-        super(FuturePreservingMap, self).__init__(input_dim=input_dim, dtype=dtype)
+    data = None
+
+    def __init__(self, output_dim, k=10, normalized_laplacian=True, neighbor_edges=True, input_dim=None, dtype=None):
+        super(FuturePreservingMapBase, self).__init__(input_dim=input_dim, output_dim=output_dim, dtype=dtype)
+        self.k = k
+        self.normalized_laplacian = normalized_laplacian
+        self.neighbor_edges = neighbor_edges
         
     
     def _train(self, x):
-        pass
+        if self.data is None:
+            self.data = x
+        else:
+            np.vstack((self.data, x))
     
     
     def _stop_training(self):
-        pass
+        """
+        Calculates the graph Laplacian.
+        """
+        
+        # initialize weight matrix W
+        N, _ = self.data.shape
+        W = scipy.sparse.dok_matrix((N, N))
+       
+        # pairwise distances of data points
+        distances = scipy.spatial.distance.pdist(self.data)
+        distances = scipy.spatial.distance.squareform(distances)
+
+        # future-preserving graph
+        for t in range(N-1):
+            neighbors = np.argsort(distances[t+1])
+            for v in neighbors[0:self.k+1]:
+                u = v-1     # predecessor of neighbor
+                if t != u:  # no self-connections
+                    W[t,u] = 1
+                    W[u,t] = 1
+                    
+        # k-nearest-neighbor graph for regularization
+        if self.neighbor_edges:
+            for i in range(N):
+                neighbors = np.argsort(distances[i])
+                for j in neighbors[0:self.k+1]:
+                    if i != j:
+                        W[i,j] = 1
+                        W[j,i] = 1
+            
+        # graph Laplacian
+        d = W.sum(axis=1).T
+        if self.normalized_laplacian:
+            d_inv = 1./d
+            D_inv = scipy.sparse.dia_matrix((d_inv, 0), shape=(N, N))
+            W = D_inv.dot(W)
+            D = scipy.sparse.eye(N, N)
+        else:
+            D = scipy.sparse.dia_matrix((d, 0), shape=(N, N))
+        
+        # keep matrices
+        L = D - W
+        self.W = W
+        self.D = D
+        self.L = L
+        return
+        
+
+
+class FuturePreservingMap(FuturePreservingMapBase):
+    
+    def __init__(self, output_dim, k=10, normalized_laplacian=True, neighbor_edges=True, input_dim=None, dtype=None):
+        FuturePreservingMapBase.__init__(self, output_dim=output_dim, k=k, normalized_laplacian=normalized_laplacian, neighbor_edges=neighbor_edges, input_dim=input_dim, dtype=dtype)
+
+        
+    def _stop_training(self):
+        FuturePreservingMapBase._stop_training(self)
+        if self.normalized_laplacian:
+            E, U = scipy.sparse.linalg.eigs(self.W, k=self.output_dim, which='LR')
+            self.E = E.real
+            self.U = U.real
+        else:
+            self.E, self.U = scipy.sparse.linalg.eigsh(self.L, M=self.D, sigma=0.0, k=self.output_dim, which='LR')
+        
+        
+        
+class FuturePreservingMapLinear(FuturePreservingMapBase):
+    
+    def __init__(self, output_dim, k=10, normalized_laplacian=True, neighbor_edges=True, input_dim=None, dtype=None):
+        FuturePreservingMapBase.__init__(self, output_dim=output_dim, k=k, normalized_laplacian=normalized_laplacian, neighbor_edges=neighbor_edges, input_dim=input_dim, dtype=dtype)
+    
+        
+    def _stop_training(self):
+        FuturePreservingMapBase._stop_training(self)
+        
+        # projected graph laplacian
+        D2 = self.data.T.dot(self.D.dot(self.data))
+        L2 = self.data.T.dot(self.L.dot(self.data))
+        #W2 = self.data.T.dot(self.W.dot(self.data))
+        
+        # calculate the eigen-vectors
+        print L2
+        self.E, self.U = scipy.sparse.linalg.eigs(L2, M=D2, sigma=0.0, k=self.output_dim, which='LR')
+        #self.E, self.U = scipy.sparse.linalg.eigsh(L2, M=D2, k=self.output_dim, which='SM')
+        #self.E, self.U = scipy.sparse.linalg.eigsh(W2, k=self.output_dim, which='LM')
+        return
+    
+    
+    def _execute(self, x):
+        return x.dot(self.U)
+    
+    
