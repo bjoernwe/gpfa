ba8bb838531448d2b8e80eeab952c1b664463364
Björn <bjoernwe@users.noreply.github.com>
2013-10-25 16:26:25 +0200

    example with chunks

--- src/example_2013_09_spiral_linear.py
+++ src/example_2013_09_spiral_linear.py
@@ -1,7 +1,13 @@
+"""
+An example that applies different dimensionality reductions on a swiss role
+hidden in noise.
+"""
+
 import numpy as np
 
 from matplotlib import pyplot
 from mpl_toolkits.mplot3d import Axes3D
+Axes3D
 
 import mdp
 
@@ -17,26 +23,8 @@ if __name__ == '__main__':
     expansion = 1
     noisy_dims = 50
     whitening = True
+    chunks = 1
     #noisy_steps = 3
-    
-    # data
-    env = EnvSwissRoll(sigma=0.5)
-    data0, _, labels = env.do_random_steps(num_steps=N)
-
-    # add noisy dim
-    for i in range(noisy_dims):
-        noise_complete = 1. * np.random.random(N)
-        data0 = np.insert(data0, 2, axis=1, values=noise_complete)
-
-    # expansion        
-    expansion_node = mdp.nodes.PolynomialExpansionNode(degree=expansion)
-    data = expansion_node.execute(data0)
-    
-    # whitening
-    if whitening:
-        whitening_node = mdp.nodes.WhiteningNode()
-        whitening_node.train(data)
-        data = whitening_node.execute(data)
     
     # algorithms
     models = []
@@ -51,10 +39,34 @@ if __name__ == '__main__':
     
     # learn
     for j, model in enumerate(models):
-        print "(%d)" % (j)
-    
-        model.train(data)
-        model.stop_training()
+        
+        # current algorithm
+        print "%s" % (model.__class__.__name__)
+        
+        # for every chunk
+        for _ in range(chunks):
+
+            # data
+            env = EnvSwissRoll(sigma=0.5)
+            data0, _, labels = env.do_random_steps(num_steps=N)
+        
+            # add noisy dim
+            for i in range(noisy_dims):
+                noise_complete = 1. * np.random.random(N)
+                data0 = np.insert(data0, 2, axis=1, values=noise_complete)
+        
+            # expansion        
+            expansion_node = mdp.nodes.PolynomialExpansionNode(degree=expansion)
+            data = expansion_node.execute(data0)
+            
+            # whitening
+            if whitening:
+                whitening_node = mdp.nodes.WhiteningNode()
+                whitening_node.train(data)
+                data = whitening_node.execute(data)
+            
+            # train
+            model.train(data)
     
         # plot
         data2 = model.execute(data)
@@ -62,5 +74,6 @@ if __name__ == '__main__':
         pyplot.scatter(x=data2[:,0], y=data2[:,1], c=labels, s=50, edgecolor='None')
         pyplot.title(model.__class__.__name__)
         
+    # show plot
     pyplot.show()
     
