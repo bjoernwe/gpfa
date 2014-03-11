db91b85f53f3d07c906a2aa56916efd30c7b0e7f
Björn <bjoernwe@users.noreply.github.com>
2014-03-11 18:00:54 +0100

    hierarchies

--- src/experiment_2014_03_video_hierarchy_fpp.py
+++ src/experiment_2014_03_video_hierarchy_fpp.py
@@ -1,15 +1,17 @@
 #! /usr/bin/python
-import mdp
-import mdp.nodes
-import mdp.parallel
-
-#import iterpfa
 
 import glob
 import numpy as np
 
 from scipy import ndimage
 from pylab import *
+
+import mdp
+import mdp.nodes
+import mdp.parallel
+
+import fpp
+
 
 """
 This example uses video images as input signal. The images are expected to be
@@ -25,7 +27,7 @@ if __name__ == "__main__":
     #------------------------------------------------------[ Parameters ]
     
     zoom_factor = 0.15
-    filename = 'result_sfa__FoT_18fJ2k.npy'
+    filename = 'result_fpp__FoT_18fJ2k.npy'
 
     #------------------------------------------------------[ Load Data ]
 
@@ -76,12 +78,24 @@ if __name__ == "__main__":
                                                         ignore_cover      = True)
 
     # sfa in layer 1
-    sfa_node_1a = mdp.nodes.SFANode( input_dim = switchboard_1.out_channel_dim,
-                                     output_dim = 32 )
-    expansion_1 = mdp.nodes.QuadraticExpansionNode( input_dim = sfa_node_1a.output_dim )
-    sfa_node_1b = mdp.nodes.SFANode( input_dim = expansion_1.output_dim,
-                                     output_dim = 32 )
-    sfa_node_1 = mdp.hinet.FlowNode( mdp.Flow([ sfa_node_1a, expansion_1, sfa_node_1b ]) )
+    whitening_1a = mdp.nodes.WhiteningNode(input_dim=switchboard_1.out_channel_dim)
+    fpp_node_1a = fpp.FPP(input_dim = switchboard_1.out_channel_dim,
+                          output_dim = 32,
+                          k = 5,
+                          iterations = 5,
+                          iteration_dim = 32,
+                          preserve_past = False,
+                          neighbor_graph = False)
+    expansion_1 = mdp.nodes.QuadraticExpansionNode( input_dim = fpp_node_1a.output_dim )
+    whitening_1b = mdp.nodes.WhiteningNode(input_dim=expansion_1.output_dim)
+    fpp_node_1b = fpp.FPP(input_dim = expansion_1.output_dim,
+                          output_dim = 32,
+                          k = 5,
+                          iterations = 5,
+                          iteration_dim = 32,
+                          preserve_past = False,
+                          neighbor_graph = False)
+    sfa_node_1 = mdp.hinet.FlowNode( mdp.Flow([ whitening_1a, fpp_node_1a, expansion_1, whitening_1b, fpp_node_1b ]) )
 
     # repetition of sfa node in layer 1
     sfa_layer_1 = mdp.hinet.CloneLayer( sfa_node_1, n_nodes = switchboard_1.output_channels )
@@ -95,26 +109,38 @@ if __name__ == "__main__":
 #                                                         ignore_cover      = True)
 # 
 #     # sfa in layer 2
-#     sfa_node_2a = mdp.nodes.SFANode( input_dim = switchboard_2.out_channel_dim,
-#                                      output_dim = 32 )
-#     expansion_2 = mdp.nodes.QuadraticExpansionNode( input_dim = sfa_node_2a.output_dim )
-#     sfa_node_2b = mdp.nodes.SFANode( input_dim = expansion_2.output_dim,
-#                                      output_dim = 32 )
-#     sfa_node_2 = mdp.hinet.FlowNode( mdp.Flow([ sfa_node_2a, expansion_2, sfa_node_2b ]) )
+#     whitening_2a = mdp.nodes.WhiteningNode(input_dim=switchboard_1.out_channel_dim)
+#     fpp_node_2a = fpp.FPP(input_dim = switchboard_2.out_channel_dim,
+#                           output_dim = 32,
+#                           k = 5,
+#                           iterations = 5,
+#                           iteration_dim = 32,
+#                           preserve_past = False,
+#                           neighbor_graph = False)
+#     expansion_2 = mdp.nodes.QuadraticExpansionNode( input_dim = fpp_node_2a.output_dim )
+#     whitening_2b = mdp.nodes.WhiteningNode(input_dim=switchboard_1.out_channel_dim)
+#     fpp_node_2b = fpp.FPP(input_dim = switchboard_2.out_channel_dim,
+#                           output_dim = 32,
+#                           k = 5,
+#                           iterations = 5,
+#                           iteration_dim = 32,
+#                           preserve_past = False,
+#                           neighbor_graph = False)
+#     sfa_node_2 = mdp.hinet.FlowNode( mdp.Flow([ whitening_2a, fpp_node_2a, expansion_2, whitening_2b, fpp_node_2b ]) )
 # 
 # 
 #     # repetition of sfa node in layer 2
 #     sfa_layer_2 = mdp.hinet.CloneLayer( sfa_node_2, n_nodes = switchboard_2.output_channels )
-
-
-    # switchboard for layer 3
+# 
+# 
+#     # switchboard for layer 3
 #     switchboard_3 = mdp.hinet.Rectangular2dSwitchboard( in_channels_xy    = switchboard_2.out_channels_xy,
 #                                                         field_channels_xy = (4, 4),
 #                                                         field_spacing_xy  = (2, 2),
 #                                                         in_channel_dim    = sfa_node_2.output_dim,
 #                                                         ignore_cover      = True)
-
-    # sfa in layer 3
+# 
+#     # sfa in layer 3
 #     sfa_node_3a = mdp.nodes.SFANode( input_dim = switchboard_3.out_channel_dim,
 #                                      output_dim = 32 )
 #     expansion_3 = mdp.nodes.QuadraticExpansionNode( input_dim = sfa_node_3a.output_dim )
