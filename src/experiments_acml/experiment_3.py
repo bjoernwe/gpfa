1dbabd2dc2f886bad1f8910a545576c749a9ddc8
Björn <bjoernwe@users.noreply.github.com>
2014-07-15 15:23:29 +0200

    manifold learning with linear gPFA and expansion

--- src/experiments_acml/experiment_3.py
+++ src/experiments_acml/experiment_3.py
@@ -6,42 +6,50 @@ import mdp
 
 import fpp
 
-from envs import env_swiss_roll_3d
+from envs.env_swiss_roll_3d import EnvSwissRoll3D
 
 if __name__ == '__main__':
 
     # parameters
     k = 5
     N = 5000
+    whitening = True
     iterations = 1
+    expansion_degree = 2
     seed = None
     
     # data
-    env = env_swiss_roll_3d.EnvSwissRoll3D()
-    data, _, labels = env.do_random_steps(num_steps=N)
+    env = EnvSwissRoll3D()
+    data_raw, _, labels = env.do_random_steps(num_steps=N)
+    
+    # expansion
+    expansion = mdp.nodes.PolynomialExpansionNode(degree=expansion_degree)
+    data = expansion.execute(data_raw)
 
-    # model    
-    model = fpp.FPPnl(output_dim=2,
-                      k=k,
-                      iterations=iterations,
-                      iteration_dim=10,
-                      minimize_variance=False,
-                      normalized_objective=True)
+    # whitening
+    if whitening:
+        whitening_node = mdp.nodes.WhiteningNode()
+        whitening_node.train(data)
+        data = whitening_node.execute(data)
 
-    # train & plot
-            
-    # current algorithm
-    print "%s" % (model.__class__.__name__)
+    # algorithms
+    model = fpp.FPP(output_dim=2,
+                    k=k,
+                    iterations=iterations,
+                    iteration_dim=2,
+                    minimize_variance=False,
+                    normalized_objective=True)
 
     # train
     model.train(data)
-    model.stop_training()
-    print model.U
-    #result = model.execute(data)
+    result = model.execute(data)
 
     # plot
-    #pyplot.subplot(1, 4, 2*e+m+1)
-    pyplot.scatter(x=model.U[:-1,1], y=model.U[:-1,2], c=labels, s=50, cmap=pyplot.cm.get_cmap('Blues'))
+    fig = pyplot.figure()
+    ax = fig.add_subplot(1, 2, 1, projection='3d')
+    ax.scatter(data_raw[:-1,0], data_raw[:-1,2], data_raw[:-1,1], c=labels, cmap=pyplot.cm.get_cmap('Blues'))
+    ax = fig.add_subplot(1, 2, 2)
+    ax.scatter(x=result[1:,0], y=result[1:,1], c=labels, s=50, cmap=pyplot.cm.get_cmap('Blues'))
     pyplot.title(model.__class__.__name__)
 
     # show plot
