9c7dbf735c1a22cb3622c1bc4b21242fe137099b
Björn <bjoernwe@users.noreply.github.com>
2014-07-16 17:40:09 +0200

    nlp slow

--- src/experiments_acml/experiment_nlp.py
+++ src/experiments_acml/experiment_nlp.py
@@ -13,22 +13,37 @@ if __name__ == '__main__':
     
     # read corpus
     excluded_tags = set([u'.', u',', u':', u'``', u"''", u'(', u')', u"'"])
-    words = [word[0].lower() for word in brown.tagged_words()[:10000] if word[1][0] not in excluded_tags]
-    words_set = tuple(set(words))
+    words = [word[0].lower() for word in brown.tagged_words()[:100000] if word[1][0] not in excluded_tags]
+    words_set = list(set(words))
     N = len(words_set)
     print N, 'unique words in Brown corpus.'
     
     # construct transition matrix
     words_indices = [words_set.index(word) for word in words]
-    word_indices_row = words_indices[:-1] + words_indices[1:]
-    word_indices_col = words_indices[1:] + words_indices[:-1]
-    W = scipy.sparse.coo_matrix((np.ones(len(word_indices_row)), (word_indices_row, word_indices_col)), shape=(N,N), dtype=int)
+    print 1
+    word_indices_row = words_indices[:-1]# + words_indices[1:]
+    print 2
+    word_indices_col = words_indices[1:]# + words_indices[:-1]
+    print 3
+    T = scipy.sparse.coo_matrix((np.ones(len(word_indices_row)), (word_indices_row, word_indices_col)), shape=(N,N), dtype=int)
+    #T = T.todok()
+    print 4
+    T = T.tolil()
+    print 5
     
-    W = W.todense()
+    for i in range(5):
+        print words_set[i]
+        for j in T[i,:].nonzero()[1]:
+            print '->', words_set[j], T[i,j]
+    
+    non_zero_indices_row, non_zero_indices_col = T.nonzero()
+    
+    
+    #W = W.todense()
     #print W
-    max_transition = np.unravel_index(np.argsort(W.flat)[-11], W.shape)
-    print words_set[max_transition[0]], words_set[max_transition[1]]
+    #max_transition = np.unravel_index(np.argsort(W.flat)[-11], W.shape)
+    #print words_set[max_transition[0]], words_set[max_transition[1]]
     
-    pyplot.imshow(W)
-    pyplot.show()
+    #pyplot.imshow(W)
+    #pyplot.show()
     
