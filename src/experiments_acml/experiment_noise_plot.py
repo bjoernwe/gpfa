d8cd89e98ebc9bb4243ab68d0e4a9ec6b5fa77d6
Björn <bjoernwe@users.noreply.github.com>
2014-07-22 11:56:18 +0200

    plot

--- src/experiments_acml/experiment_noise_plot.py
+++ src/experiments_acml/experiment_noise_plot.py
@@ -50,7 +50,7 @@ def experiment(data, algorithm, k, variance_graph, iterations, iteration_dim, re
         node = fpp.LPP(output_dim=2,
                    k=k,
                    normalized_objective=True)
-    elif algorithm == 'fpp':
+    elif algorithm == 'gpfa':
         node = fpp.FPP(output_dim=2,
                    k=k,
                    iterations=iterations,
@@ -118,22 +118,40 @@ def variance_of_neighbors(projected_data, neighbor_list):
     
     N = projected_data.shape[0]
     
-    performance = 0
-    number_of_edges = 0
+    cov_matrix = mdp.utils.CovarianceMatrix()
+    
+    #performance = 0
+    #number_of_edges = 0
     for t, neighborhood in enumerate(neighbor_list):
-        neighborhood = np.setdiff1d(neighborhood, np.array([N-1]), assume_unique=True)
         if len(neighborhood) == 0:
             continue
         delta_vectors = projected_data[neighborhood] - projected_data[t]
-        squared_distances = np.diag(delta_vectors.dot(delta_vectors.T))
-        assert len(neighborhood) >= 1
-        assert np.all(np.isfinite(delta_vectors))
-        assert len(squared_distances) == len(neighborhood)
-        performance += np.sum(squared_distances)
-        number_of_edges += len(neighborhood)
-    performance = np.sqrt(performance / (number_of_edges - 1))
+        cov_matrix.update(delta_vectors)
+        #squared_distances = np.diag(delta_vectors.dot(delta_vectors.T))
+        #assert len(neighborhood) >= 1
+        #assert np.all(np.isfinite(delta_vectors))
+        #assert len(squared_distances) == len(neighborhood)
+        #performance += np.sum(squared_distances)
+        #number_of_edges += len(neighborhood)
+    #performance = np.sqrt(performance / (number_of_edges - 1))
+    covariance, mean, number_of_edges = cov_matrix.fix()
+    performance = np.sum(np.linalg.eigh(covariance)[0])
+    #print covariance
+    #print np.linalg.eigh(covariance)
     return performance
 
+
+def get_handle_lists(l):
+    """
+    Found this here:
+    http://nbviewer.ipython.org/gist/leejjoon/5603703
+    """
+    tree = l._legend_box.get_children()[1]
+
+    for column in tree.get_children():
+        for row in column.get_children():
+            yield row.get_children()[0].get_children()
+    
     
 if __name__ == '__main__':
     
@@ -141,50 +159,95 @@ if __name__ == '__main__':
     steps = 2000
     variance_graph = False
     do_pca = False
-    trials = 2
+    trials = 1
     noisy_dims = 20
     additive_noise = 0
     iter_dims = [None, 2, 5]#, 20, 50, 100]
     iterations_list = [1, 2, 3, 4, 5]#, 7, 10]#, 15, 20]#, 50, 100, 200, 500]
-    noisy_dims_list = [0, 50, 100, 150, 200, 250, 300]
+    noisy_dims_list = [0, 1]#, 2, 5, 10]#, 20, 50, 100, 150]#, 200, 250, 300]
     
-    results = np.zeros((len(noisy_dims_list),trials))
-        
-    for r in range(trials):
-            
-        env = envs.env_cube.EnvCube()
-        data, _, _ = env.do_random_steps(num_steps=steps)
-        
-        # baseline
-        result = experiment(data=data, 
-                            algorithm='fpp', 
-                            k=k, 
-                            variance_graph=variance_graph, 
-                            iterations=5, 
-                            iteration_dim=2, 
-                            reduce_variance=False, 
-                            whitening=True, 
-                            additional_noise_dim=0)
-        
-        neighbor_list = calc_neighbor_list(result)
-        
-        for d, noisy_dims in enumerate(noisy_dims_list):
+    results = {}
+    
+    for a, algorithm in enumerate(['lpp', 'gpfa']):
 
-            print noisy_dims, 'noisy dimensions, trial', (r+1)
+        results[algorithm] = {}
+        
+        for g, graph_text in enumerate(['standard_graph', 'variance_graph']):
             
-            result = experiment(data=data, 
-                                algorithm='fpp', 
-                                k=k, 
-                                variance_graph=variance_graph, 
-                                iterations=5, 
-                                iteration_dim=2, 
-                                reduce_variance=False, 
-                                whitening=True, 
-                                additional_noise_dim=noisy_dims)
+            if algorithm == 'lpp' and graph_text == 'variance_graph':
+                continue
+                    
+            results[algorithm][graph_text] = {}
+            results[algorithm][graph_text]['neighbors'] = np.zeros((len(noisy_dims_list),trials))
+            results[algorithm][graph_text]['future'] = np.zeros((len(noisy_dims_list),trials))
+                
+            for r in range(trials):
+                    
+                env = envs.env_cube.EnvCube()
+                data, _, _ = env.do_random_steps(num_steps=steps)
+                
+                # baseline
+                result = experiment(data=data, 
+                                    algorithm=algorithm, 
+                                    k=k, 
+                                    variance_graph=bool(g), 
+                                    iterations=5, 
+                                    iteration_dim=2, 
+                                    reduce_variance=False, 
+                                    whitening=True, 
+                                    additional_noise_dim=0)
+                
+                neighbor_list = calc_neighbor_list(result)
+                
+                for d, noisy_dims in enumerate(noisy_dims_list):
+        
+                    print noisy_dims, 'noisy dimensions, trial', (r+1)
+                    
+                    result = experiment(data=data, 
+                                        algorithm=algorithm, 
+                                        k=k, 
+                                        variance_graph=bool(g), 
+                                        iterations=5, 
+                                        iteration_dim=2, 
+                                        reduce_variance=False, 
+                                        whitening=True, 
+                                        additional_noise_dim=noisy_dims)
+                    
+                    results[algorithm][graph_text]['neighbors'][d,r] = variance_of_neighbors(result, neighbor_list)
+                    results[algorithm][graph_text]['future'][d,r] = variance_of_future(result, neighbor_list)
             
-            results[d,r] = variance_of_future(result, neighbor_list)
-    
-    pyplot.errorbar(x=noisy_dims_list, y=np.mean(results, axis=1), yerr=np.std(results, axis=1))
+            facecolor = 'white' if g == 1 else None
+            color = 'b' if a == 1 else 'r'
+            marker = 'o' if a == 1 else '^'
+            
+            pyplot.errorbar(x=noisy_dims_list, 
+                            y=np.mean(results[algorithm][graph_text]['neighbors'], axis=1), 
+                            yerr=np.std(results[algorithm][graph_text]['neighbors'], axis=1), 
+                            markersize=8, 
+                            marker=marker, 
+                            markerfacecolor=facecolor, 
+                            linestyle='-',
+                            color=color)
+            pyplot.errorbar(x=noisy_dims_list, 
+                            y=np.mean(results[algorithm][graph_text]['future'], axis=1), 
+                            yerr=np.std(results[algorithm][graph_text]['future'], axis=1), 
+                            markersize=8, marker=marker, 
+                            markerfacecolor=facecolor, 
+                            linestyle='--',
+                            color=color)
+
+    marker_lpp  = pyplot.Line2D((0,1),(0,0), color='black', linestyle='', marker='^', markersize=8)
+    marker_gpfa_1 = pyplot.Line2D((0,1),(0,0), color='black', linestyle='', marker='o', markersize=8)
+    marker_gpfa_2 = pyplot.Line2D((0,1),(0,0), color='black', linestyle='', marker='o', markersize=8, markerfacecolor='none')
+    line_neighbors = pyplot.Line2D((0,1),(0,0), color='black', linestyle='-')
+    line_future = pyplot.Line2D((0,1),(0,0), color='black', linestyle='--')
+    l = pyplot.legend([marker_lpp, marker_gpfa_1, marker_gpfa_2, line_neighbors, line_future], ['LPP', 'gPFA (variant 1)', 'gPFA (variant 2)', 'dist. of neighb.', 'dist. of neighb. in next step'])
+    handles_list = list(get_handle_lists(l))
+    handles = handles_list[0]
+    print [method for method in dir(handles[0]) if callable(getattr(handles[0], method))]
+    #print handles[0].set_markerfacecolor('b')
+    #print handles[1].set_markerfacecolor('white')
+    #handles[1].set_markerfacecolor(['none', 'full'])
     pyplot.show()
 
 #     results = {}
