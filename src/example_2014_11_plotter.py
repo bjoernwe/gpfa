da1aa1d3eeb27aae1e71b92d7030c718de34e87a
Björn <bjoernwe@users.noreply.github.com>
2014-11-07 18:53:01 +0100

    working version

--- src/example_2014_11_plotter.py
+++ src/example_2014_11_plotter.py
@@ -1,27 +1,24 @@
 import collections
 import functools
-import numpy as np
 import multiprocessing
+import numpy as np
+import os
 
-
-def my_func(a, b, c=False):
-    N = 3000
-    A = np.random.randn(N, N)
-    _, _ = np.linalg.eig(A)
-    print 'a:', a
-    print 'b:', b
-    print 'c:', c
-    print '\n'
+from matplotlib import pyplot as plt
 
 
 """
 A simple wrapper for function f that allows having a specific argument
-('arg_name') as the first argument.
+('arg_name') as the first argument. The argument 'niceness' is removed from
+kwargs and used to increment the niceness of the current process (default: 10).
+Also the NumPy's random number generator is initialized with a new seed.
 """
-def f_wrapper(arg, arg_name, f, **kwargs):
+def _f_wrapper(arg, arg_name, f, **kwargs):
+    os.nice(kwargs.pop('niceness', 10))
+    np.random.seed()
     kwargs[arg_name] = arg
-    print kwargs
     return f(**kwargs)
+
 
 
 """
@@ -29,28 +26,81 @@ Plots the real-valued function f using its given arguments. One of the argument
 is expected to be an iterable, which is used for the x-axis.
 """
 def plot(f, **kwargs):
+    
+    # look for iterable arguments
     iterable_arguments = [k for (k, v) in kwargs.items() 
                           if isinstance(v, collections.Iterable)]
+
     if len(iterable_arguments) == 0:
+
         print 'Warning: No iterable argument found for plotting.'
         return
+
     elif len(iterable_arguments) >= 2:
+
         print 'Warning: More than one iterable argument found for plotting.'
         return
+
     else:
-        arg_name = iterable_arguments[0]
-        arg = kwargs.pop(arg_name)
-        f_partial = functools.partial(f_wrapper, arg_name=arg_name, f=f,
+
+        # extract arguments for plotter
+        arg_name    = iterable_arguments[0]
+        arg         = kwargs.pop(arg_name)
+        processes   = kwargs.pop('processes', multiprocessing.cpu_count())
+        show_plot   = kwargs.pop('show_plot', True)
+        repetitions = kwargs.pop('repetitions', 1)
+
+        # wrap function f
+        f_partial = functools.partial(_f_wrapper, arg_name=arg_name, f=f,
                                       **kwargs)
-        pool = multiprocessing.Pool()
+
+        # prepare argument list for repetitions
+        if repetitions > 1:
+            old_arg = arg
+            arg = np.array(arg)
+            arg = np.repeat(arg, repetitions)
+
+        # start a pool of processes
+        pool = multiprocessing.Pool(processes=processes)
         result = pool.map(f_partial, arg)
         pool.close()
         pool.join()
-    return result
+
+        # either errorbar plot or regular plot
+        if repetitions > 1:
+            arg = old_arg
+            result = np.reshape(result, (len(arg), repetitions))
+            plt.errorbar(arg, np.mean(result, axis=1),
+                         yerr=np.std(result, axis=1))
+        else:
+            plt.plot(arg, result)
+
+        # describe plot
+        plt.xlabel(arg_name)
+
+        # show plot
+        if show_plot:
+            plt.show()
+
+        return result
+
+    return
+
+
+"""
+A simple example function with two arguments x and y.
+"""
+def _example_func(x, y):
+    fx = x**2 / 10
+    fy = np.sin(y)
+    return fx + fy + .5 * np.random.randn()
 
 
 def main():
-    plot(my_func, a=-1, b=[1, 2, 3, 4, 5], c=True)
+    plot(_example_func, x=0, y=range(10), repetitions=10)
+    #plot(_example_func, x=range(10), y=0, repetitions=100, show_plot=False)
+    #plot(_example_func, x=0, y=range(10), repetitions=100, show_plot=False)
+    #plt.show()
 
 
 if __name__ == '__main__':
