ced1f69b360aa514faf637d2df1e71ba53e9c96b
Björn <bjoernwe@users.noreply.github.com>
2014-12-04 12:36:40 +0100

    float k for epsilon-neighborhood and updated predictability measures

--- src/gpfa.py
+++ src/gpfa.py
@@ -8,7 +8,7 @@ import mdp
 
 
 
-def calc_predictability_star(data, k):
+def calc_predictability_graph_star(data, k):
 
     # pairwise distances of data points
     N, _ = data.shape
@@ -25,7 +25,10 @@ def calc_predictability_star(data, k):
 
 
 
-def calc_predictability_var(data, k):
+def calc_predictability_graph_var(data, k):
+
+    if data.ndim == 1:
+        data = np.array(data, ndmin=2).T
 
     # pairwise distances of data points
     N, _ = data.shape
@@ -36,6 +39,30 @@ def calc_predictability_var(data, k):
     v = 0
     for t in range(N-1):
         v += np.mean([np.linalg.norm(data[i+1] - data[j+1])**2 for i, j in itertools.combinations(neighbors[t], 2) if i+1 < N and j+1 < N])
+    v /= N-1
+    return .5*v
+
+
+
+def calc_predictability_det_var(data, k):
+
+    if data.ndim == 1:
+        data = np.array(data, ndmin=2).T
+
+    # pairwise distances of data points
+    N, _ = data.shape
+    distances = scipy.spatial.distance.pdist(data)
+    distances = scipy.spatial.distance.squareform(distances)
+    neighbors = [np.argsort(distances[i])[:k+1] for i in xrange(N)]
+
+    v = 0
+    for t in range(N-1):
+        successors = neighbors[t] + 1
+        successors = successors[successors<N]
+        suc_dat = data[successors]
+        covariance = np.array(np.cov(suc_dat.T), ndmin=2)
+        det = np.linalg.det(covariance)
+        v += det
     v /= N-1
     return v
 
@@ -160,7 +187,12 @@ class gPFA(mdp.Node):
             # pairwise distances of data points
             distances = scipy.spatial.distance.pdist(y)
             distances = scipy.spatial.distance.squareform(distances)
-            neighbors = [np.argsort(distances[i])[:self.k+1] for i in xrange(N)]
+            if isinstance(self.k, int):
+                neighbors = [np.argsort(distances[i])[:self.k+1] for i in xrange(N)]
+            elif isinstance(self.k, float):
+                neighbors = [np.array([j for (j, d) in enumerate(distances[i]) if d <= self.k], dtype=int) for i in xrange(N)]
+            else:
+                assert False
 
             # future-preserving graph
             index_list = []
@@ -185,7 +217,7 @@ class gPFA(mdp.Node):
             # count edges only once
             if not self.variance_graph:
                 index_list = list(set(index_list))
-
+                
             # weight matrix from index list
             index_list = np.array(index_list)
             W = scipy.sparse.coo_matrix((np.ones(index_list.shape[0]), (index_list[:,0], index_list[:,1])), shape=(N+1,N+1))
