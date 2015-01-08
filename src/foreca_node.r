3156d0eaae7503d9e5a413caeb1d57f34d655c05
Björn <bjoernwe@users.noreply.github.com>
2015-01-08 16:28:21 +0100

    adaption for ForeCA_0.2.0

--- src/foreca_node.r
+++ src/foreca_node.r
@@ -4,7 +4,7 @@ args <- commandArgs(trailingOnly = TRUE)
 arg_id <- args[1]
 arg_dim <- as.numeric(args[2])
 
-X <- read.csv(sprintf('foreca_node_train_%s.csv', arg_id), header=FALSE)
+X <- ts(read.csv(sprintf('foreca_node_train_%s.csv', arg_id), header=FALSE))
 ff <- foreca(X, n.comp=arg_dim)
 summary(ff)
 print(ff$loadings)
