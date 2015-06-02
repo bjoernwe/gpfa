06bc49ae428d52d026d63b176dbcd15044fe2986
Björn <bjoernwe@users.noreply.github.com>
2015-06-02 18:49:49 +0200

    new parameters

--- src/experiments_acml/experiment_face.py
+++ src/experiments_acml/experiment_face.py
@@ -12,27 +12,28 @@ import experiments.experiment_base as eb
 
 def main():
     eep.plot(eb.prediction_error,
-             algorithm=['foreca', 'pfa', 'gpfa-1', 'gpfa-2', 'lpp', 'random'], 
+             algorithm=['sfa', 'foreca', 'pfa', 'gpfa-1', 'gpfa-2', 'lpp'],#, 'random'], 
              N=2000, 
-             k=5, #[3, 5, 10, 15, 20, 30, 50], 
-             p=2, 
+             k=[3, 5, 10, 15, 20, 30, 40, 50], 
+             p=1, 
              K=1, 
-             iterations=10, 
+             iterations=100,#[100, 200, 300, 400], 
              noisy_dims=0, 
              neighborhood_graph=True,
              weighted_edges=True, 
-             keep_variance=np.arange(.85, .95, .01), 
-             iteration_dim=2, 
+             keep_variance=.9,#np.arange(.85, .96, .01), 
+             iteration_dim=1, 
+             output_dim=1, 
              data='face', 
-             measure='avg_det_of_cov', 
+             measure='det_of_avg_cov', 
              repetitions=1, 
              processes=None, 
              argument_order=None, 
-             cachedir='/tmp', 
+             cachedir='/scratch/weghebvc', 
              plot_elapsed_time=False, 
              show_plot=False, 
              save_plot=False)
-    plt.gca().set_yscale('log')
+    #plt.gca().set_yscale('log')
     plt.show()
 
 
