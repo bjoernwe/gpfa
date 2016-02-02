1f3f6acceb37f63b2ce8d8239ba856bca39f62d1
Björn <bjoernwe@users.noreply.github.com>
2016-02-02 16:05:54 +0100

    new experiments

--- src/experiments_ecml/experiment_eeg.py
+++ src/experiments_ecml/experiment_eeg.py
@@ -8,57 +8,43 @@ import explot as ep
 
 import experiments.experiment_base as eb
 
+import plot
 
 
-def experiment(N=2500, keep_variance=1., k=2, p=2, iterations=60, output_dim=2):
-    
-    #plt.figure()
-    ep.plot(eb.prediction_error,
-            algorithm=['pfa', 'gcfa-1', 'gcfa-2'], 
-            N=N, 
-            keep_variance=keep_variance,
-            k=k,
-            p=p, 
-            K=0, 
-            seed=0,
-            iterations=iterations,
-            noisy_dims=0, 
-            neighborhood_graph=False,
-            weighted_edges=True, 
-            output_dim=output_dim, 
-            data='eeg',
-            measure='trace_of_avg_cov', 
-            reverse_error=False,
-            repetitions=1, 
-            processes=None,
-            argument_order=['N', 'iterations'], 
-            cachedir='/scratch/weghebvc',
-            plot_elapsed_time=False, 
-            show_plot=False, 
-            save_plot_path='./plots')
-    #plt.gca().set_yscale('log')
-    #plt.show()
+
+def plot_experiment(N=2500, k=2, p=2, K=0, keep_variance=.98, iterations=50, output_dim=2, include_random=False, include_foreca=False, include_gcfa=True, x_offset=0, y_label=True, legend=False):
+    plot.plot_experiment(data='eeg', 
+                         N=N, 
+                         k=k, 
+                         p=p, 
+                         P=p,
+                         K=K, 
+                         noisy_dims=0,
+                         keep_variance=keep_variance, 
+                         iterations=iterations, 
+                         output_dim=output_dim,
+                         repetitions=1, 
+                         include_random=include_random, 
+                         include_foreca=include_foreca, 
+                         include_gcfa=include_gcfa, 
+                         x_offset=x_offset, 
+                         y_label=y_label, 
+                         legend=legend,
+                         seed=0)
     
     
     
 def main():
-    
-    # mario
-    #plt.subplot(2,2,1)
-    #experiment(p=[1,2,3,4])
-    #plt.subplot(1,2,2)
-    #experiment(output_dim=[1,2,3,4])
-    #plt.subplot(2, 2, 1)
-    experiment(keep_variance=list(np.arange(.85, 1., .01)))
-    #plt.subplot(2, 2, 2)
-    #experiment(N=[1500, 2000, 2500])
+    #plt.figure()
+    plt.subplot(2, 2, 1)
+    plot_experiment(keep_variance=list(np.arange(.80, 1.01, .02)))
+    plt.subplot(2, 2, 2)
+    #plot_experiment(N=[1500, 1750, 2000, 2250, 2500])
     #plt.subplot(2, 2, 3)
-    #experiment(iterations=[20, 40, 60, 80, 100])
-    #plt.subplot(2, 2, 4)
-    #experiment(k=[2]+range(5,51,10))
-
+    #plot_experiment(iterations=[20, 40, 60, 80, 100, 150])
+    #plt.subplot(1, 2, 2)
+    plot_experiment(k=range(2,5) + range(5,21,5))
     plt.show()
-
 
 
 if __name__ == '__main__':
--- src/experiments_ecml/experiment_face.py
+++ src/experiments_ecml/experiment_face.py
@@ -8,54 +8,57 @@ import explot as ep
 
 import experiments.experiment_base as eb
 
+import plot
 
 
-def experiment(N=2000, keep_variance=.95, k=2, p=2, iterations=50, output_dim=2):
-    
-    #plt.figure()
-    ep.plot(eb.prediction_error,
-            algorithm=['pfa', 'gcfa-1', 'gcfa-2'], 
-            N=N, 
-            keep_variance=keep_variance,
-            k=k,
-            p=p, 
-            K=1, 
-            seed=0,
-            iterations=iterations,
-            noisy_dims=0, 
-            neighborhood_graph=False,
-            weighted_edges=True, 
-            output_dim=output_dim, 
-            data='face',
-            measure='trace_of_avg_cov', 
-            reverse_error=False,
-            repetitions=1, 
-            processes=None,
-            argument_order=['N', 'iterations'], 
-            cachedir='/scratch/weghebvc',
-            plot_elapsed_time=False, 
-            show_plot=False, 
-            save_plot_path='./plots')
-    #plt.gca().set_yscale('log')
-    #plt.show()
+
+def plot_experiment(N=1500, k=2, p=2, K=0, keep_variance=.98, iterations=50, output_dim=2, include_random=False, include_foreca=False, include_gcfa=True, x_offset=0, y_label=True, legend=False):
+    plot.plot_experiment(data='face', 
+                         N=N, 
+                         k=k, 
+                         p=p, 
+                         P=p,
+                         K=K, 
+                         noisy_dims=0,
+                         keep_variance=keep_variance, 
+                         iterations=iterations, 
+                         output_dim=output_dim,
+                         repetitions=1, 
+                         include_random=include_random, 
+                         include_foreca=include_foreca, 
+                         include_gcfa=include_gcfa, 
+                         x_offset=x_offset, 
+                         y_label=y_label, 
+                         legend=legend,
+                         seed=0)
     
     
     
 def main():
-    
-    # mario
-    plt.figure()
-    #plt.subplot(1, 2, 1)
-    experiment(keep_variance=list(np.arange(.7, 1., .01)))
-    #plt.subplot(2, 2, 2)
-    #experiment(N=[1500, 1750, 2000, 2250, 2500])
+    #plt.figure()
+    plt.subplot(2, 2, 1)
+    plot_experiment(keep_variance=list(np.arange(.80, 1.01, .02)))
+    plt.subplot(2, 2, 2)
+    #plot_experiment(N=[1500, 1750, 2000, 2250, 2500])
     #plt.subplot(2, 2, 3)
-    #experiment(iterations=[20, 40, 60, 80, 100, 150])
+    #plot_experiment(iterations=[20, 40, 60, 80, 100, 150])
     #plt.subplot(1, 2, 2)
-    #experiment(k=[2]+range(5,11,5))
+    plot_experiment(k=range(2,5) + range(5,21,5))
     plt.show()
 
 
-
+def main_plot():
+    plt.figure()
+    plot_experiment(keep_variance=list(np.arange(.80, 1.01, .02)), output_dim=3)
+    plt.figure()
+    plot_experiment(keep_variance=list(np.arange(.80, 1.01, .02)), output_dim=6)
+    plt.figure()
+    plot_experiment(keep_variance=list(np.arange(.80, 1.01, .02)), output_dim=9)
+    plt.figure()
+    plot_experiment(keep_variance=list(np.arange(.80, 1.01, .02)), output_dim=12, legend=True)
+    plt.show()
+    
+    
 if __name__ == '__main__':
     main()
+    #main_plot()
