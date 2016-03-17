aa2932f68ccb9806e8988de9f0172d82b3107ac5
Björn <bjoernwe@users.noreply.github.com>
2016-03-17 15:52:07 +0100

    foreca outputs white signals

--- src/foreca/foreca_node.py
+++ src/foreca/foreca_node.py
@@ -11,7 +11,17 @@ mem = joblib.Memory(cachedir='/scratch/weghebvc', verbose=1)
 
 
 @mem.cache
-def train(x, output_dim, seed):
+def train(x, output_dim, whitening, seed):
+    
+        # whitening
+        m = None
+        W = None
+        if whitening:
+            whitening_node = mdp.nodes.WhiteningNode(reduce=True)
+            whitening_node.train(x)
+            x = whitening_node.execute(x)
+            m = whitening_node.avg
+            W = whitening_node.v
     
         # save training data in CSV
         cwd = os.getcwd()
@@ -23,14 +33,14 @@ def train(x, output_dim, seed):
         # run R script and load result (extraction matrix)
         output_dim = max(2, output_dim)
         call(['Rscript', '%s/foreca_node.r' % fdir, run_id, str(output_dim), cwd])
-        W = np.loadtxt('%s/foreca_node_result_%s.csv' % (cwd, run_id))
+        U = np.loadtxt('%s/foreca_node_result_%s.csv' % (cwd, run_id))
         if output_dim < 2:
-            W = W[:,0:1]
+            U = U[:,0:1]
 
         # clean files
         call(['rm', '%s/foreca_node_train_%s.csv' % (cwd, run_id)])
         call(['rm', '%s/foreca_node_result_%s.csv' % (cwd, run_id)])
-        return W
+        return m, W, U
 
 
 
@@ -39,9 +49,12 @@ class ForeCA(mdp.Node):
     A wrapper node for the ForeCA implementation in R.
     '''
 
-    def __init__(self, output_dim, input_dim=None, dtype=None, seed=None):
+    def __init__(self, output_dim, input_dim=None, whitening=True, dtype=None, seed=None):
         super(ForeCA, self).__init__(input_dim=input_dim, output_dim=output_dim, dtype=dtype)
+        self.whitening = whitening
+        self.m = None
         self.W = None
+        self.U = None
         self.seed = seed
         #self.rnd = np.random.RandomState(seed)
         return
@@ -49,7 +62,7 @@ class ForeCA(mdp.Node):
         
         
     def _train(self, x):
-        self.W = train(x=x, output_dim=self.output_dim, seed=self.seed)
+        self.m, self.W, self.U = train(x=x, output_dim=self.output_dim, whitening=self.whitening, seed=self.seed)
         return
     
     
@@ -60,5 +73,18 @@ class ForeCA(mdp.Node):
     
     
     def _execute(self, x):
-        return x.dot(self.W)
+        if self.m is not None:
+            x = x - self.m
+            x = x.dot(self.W)
+        return x.dot(self.U)
 
+
+
+if __name__ == '__main__':
+    x = np.random.randn(100,10)
+    x = np.hstack([x,x])
+    foreca = ForeCA(output_dim=2)
+    foreca.train(x)
+    y = foreca.execute(x)
+    print np.cov(y.T)
+    
