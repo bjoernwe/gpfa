13d19056e53684eb89c463ce0d1cfdb729a6368e
Björn <bjoernwe@users.noreply.github.com>
2017-03-15 11:57:48 +0100

    update for angle plot

--- src/experiments_proxy/plot_angles.py
+++ src/experiments_proxy/plot_angles.py
@@ -11,11 +11,19 @@ def main():
 
     mkl.set_num_threads(1)
 
-    algs = [eb.Algorithms.Random,
+    plot_alg_names = {eb.Algorithms.Random: 'Random',
+                      eb.Algorithms.SFA:    'SFA',
+                      eb.Algorithms.SFFA:   "SFA'",
+                      eb.Algorithms.ForeCA: 'ForeCA',
+                      eb.Algorithms.PFA:    'PFA',
+                      eb.Algorithms.GPFA2:  'GPFA',
+                      }
+
+    algs = [#eb.Algorithms.Random,
             #eb.Algorithms.ForeCA,
             #eb.Algorithms.SFA,
-            eb.Algorithms.SFFA,
-            eb.Algorithms.PFA,
+            #eb.Algorithms.SFFA,
+            #eb.Algorithms.PFA,
             eb.Algorithms.GPFA2
             ]
 
@@ -28,13 +36,14 @@ def main():
         
     for _, alg in enumerate(algs):
             
-        plt.figure()
-        plt.suptitle(alg)
+        plt.figure(figsize=(10,6))
+        plt.suptitle(plot_alg_names[alg])
             
         for ids, dataset_args in enumerate(parameters.dataset_args):
 
             for min_principal_angle in [False, True]:
 
+                env = dataset_args['env']
                 dataset = dataset_args['dataset']
                 if not dataset in results_angle[alg][min_principal_angle]:
                     continue
@@ -45,15 +54,21 @@ def main():
                 values = results_angle[alg][min_principal_angle][dataset].values
                 d, _ = values.shape
                 plt.errorbar(x=range(1,d+1), y=np.mean(values, axis=1), yerr=np.std(values, axis=1))
-                plt.xlim(.5, d+.5)
-                plt.ylim(-.1, np.pi/2+.1)
+                plt.xlim(.5, 10+.5)
+                plt.ylim(-.2, np.pi/2+.2)
+                if ids % 4 == 0:
+                    plt.ylabel('angle')
+                else:
+                    plt.gca().set_yticklabels([])
+                if ids >= 12:
+                    plt.xlabel('# dimensions')
+                else:
+                    plt.gca().set_xticklabels([])
                     
                 # title
-                #dataset_str = '%s<%s>' % (dataset_args['env'], dataset_args['dataset'])
-                dataset_str = '%s' % (dataset_args['dataset'])
-                plt.title(dataset_str)
+                plt.title(eb.get_dataset_name(env=env, ds=dataset, latex=False), fontsize=12)
 
-        plt.subplots_adjust(hspace=.4, wspace=.3, left=0.02, right=.98, bottom=.02, top=.95)
+        plt.subplots_adjust(hspace=.4, wspace=.15, left=0.07, right=.96, bottom=.08, top=.92)
             
     plt.show()
 
