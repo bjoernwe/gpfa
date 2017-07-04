d9f03269a0ae679b50549d0ca73d90feb2ac1ea2
Björn <bjoernwe@users.noreply.github.com>
2017-07-04 15:06:43 +0200

    test for ForeCA, PFA, and GPFA

--- src/nodes_test.py
+++ src/nodes_test.py
@@ -1,7 +1,12 @@
 import matplotlib.pyplot as plt
+import mdp
 import numpy as np
 import unittest
 
+import PFANodeMDP
+
+import foreca.foreca_node
+import gpfa_node
 import sfa_node
 
 
@@ -19,7 +24,7 @@ class NodesTestCase(unittest.TestCase):
         n_noisy_dims = 5
 
         # generate sine wave and alternating signal
-        cls.signal1 = 10 * np.sin(np.linspace(0, 4*np.pi, num=n_samples))
+        cls.signal1 = np.sqrt(2) * np.sin(np.linspace(0, 4*np.pi, num=n_samples))
         cls.signal2 = np.ones(n_samples)
         cls.signal2[::2] = -1
         signals = np.stack([cls.signal1, cls.signal2], axis=1)
@@ -29,6 +34,7 @@ class NodesTestCase(unittest.TestCase):
 
         # concatenate
         data = np.concatenate([signals[:,[0]], noise, signals[:,[1]]], axis=1)
+
         # mix signals via rotation matrix (45 degrees)
         R = np.array([[.7071, -.7071],[.7071, .7071]])
         M = np.eye(n_noisy_dims + 2)
@@ -40,18 +46,80 @@ class NodesTestCase(unittest.TestCase):
 
     def test_sfa(self):
         # sine wave & noise
-        sfa = sfa_node.SFA(output_dim=2, include_fast_signals=False)
+        sfa = sfa_node.SFA(output_dim=3, include_fast_signals=False)
         sfa.train(self.data)
         signals = sfa.execute(self.data)
         assert self._is_similar(signals[:,0], self.signal1)
         assert not self._is_similar(signals[:,1], self.signal2)
+        assert not self._is_similar(signals[:,2], self.signal1)
+        assert not self._is_similar(signals[:,2], self.signal2)
 
+    def test_sfa_fast(self):
         # fast & sine wave
-        sfa = sfa_node.SFA(output_dim=2, include_fast_signals=True)
+        sfa = sfa_node.SFA(output_dim=3, include_fast_signals=True)
         sfa.train(self.data)
         signals = sfa.execute(self.data)
         assert self._is_similar(signals[:,0], self.signal2)
         assert self._is_similar(signals[:,1], self.signal1)
+        assert not self._is_similar(signals[:,2], self.signal1)
+        assert not self._is_similar(signals[:,2], self.signal2)
+
+    def test_foreca(self):
+        # without whitening
+        foreca_node = foreca.foreca_node.ForeCA(output_dim=3, whitening=False)
+        foreca_node.train(self.data)
+        signals = foreca_node.execute(self.data)
+        assert self._is_similar(signals[:,0], self.signal2)
+        assert self._is_similar(signals[:,1], self.signal1)
+        assert not self._is_similar(signals[:,2], self.signal1)
+        assert not self._is_similar(signals[:,2], self.signal2)
+
+    def test_foreca_whitening(self):
+        # with whitening
+        foreca_node = foreca.foreca_node.ForeCA(output_dim=3, whitening=True)
+        foreca_node.train(self.data)
+        signals = foreca_node.execute(self.data)
+        assert self._is_similar(signals[:,0], self.signal2)
+        assert self._is_similar(signals[:,1], self.signal1)
+        assert not self._is_similar(signals[:,2], self.signal1)
+        assert not self._is_similar(signals[:,2], self.signal2)
+
+    def test_pfa(self):
+        pfa = PFANodeMDP.PFANode(output_dim=3, p=1, k=1)
+        pfa.train(self.data)
+        signals = pfa.execute(self.data)
+        assert self._is_similar(signals[:,0], self.signal2)
+        assert self._is_similar(signals[:,1], self.signal1)
+        assert not self._is_similar(signals[:,2], self.signal1)
+        assert not self._is_similar(signals[:,2], self.signal2)
+
+    def test_gpfa(self):
+        # non-whitened data
+        gpfa = gpfa_node.GPFA(output_dim=3, p=2, k=5, iterations=10)
+        gpfa.train(self.data)
+        signals = gpfa.execute(self.data)
+        assert self._is_similar(signals[:,0], self.signal2)
+        assert self._is_similar(signals[:,1], self.signal1)
+        assert not self._is_similar(signals[:,2], self.signal1)
+        assert not self._is_similar(signals[:,2], self.signal2)
+
+    def test_gpfa_white(self):
+        # whitened data
+        whitening = mdp.nodes.WhiteningNode()
+        whitening.train(self.data)
+        data_white = whitening.execute(self.data)
+        gpfa = gpfa_node.GPFA(output_dim=3, p=2, k=5, iterations=10)
+        gpfa.train(data_white, is_whitened=True)
+        signals = gpfa.execute(data_white)
+        assert self._is_similar(signals[:,0], self.signal2)
+        assert self._is_similar(signals[:,1], self.signal1)
+        assert not self._is_similar(signals[:,2], self.signal1)
+        assert not self._is_similar(signals[:,2], self.signal2)
+
+    def test_gpfa_white_exception(self):
+        # detection of non-white data
+        gpfa = gpfa_node.GPFA(output_dim=3, p=2, k=5, iterations=10)
+        self.assertRaises(ValueError, gpfa.train, x=self.data, is_whitened=True)
 
     def _is_similar(self, signal1, signal2, rtol=0, atol=1e-1):
         """
